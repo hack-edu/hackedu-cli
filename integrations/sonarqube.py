516d6efd3c93cbe1366461a84ada45ead39afb75
Alex <alexdaro34@gmail.com>
2021-04-14 17:54:09 -0700

    feat: progress bar for sync command. include sonarqube url as config option

--- integrations/sonarqube.py
+++ integrations/sonarqube.py
@@ -1,4 +1,5 @@
 from sonarqube import SonarQubeClient, SonarEnterpriseClient, SonarCloudClient
+import click
 import re
 
 class SonarqubeBase(object):
@@ -12,21 +13,21 @@ class SonarqubeBase(object):
         self.edition = edition
 
         if self.username and self.password:
-            if self.edition == 'community':
+            if self.edition == "community":
                 self.client = SonarQubeClient(sonarqube_url=self.url, username=self.username, password=self.password)
-            elif self.edition == 'enterprise':
+            elif self.edition == "enterprise":
                 self.client = SonarEnterpriseClient(sonarqube_url=self.url, username=self.username, password=self.password)
-            elif self.edition == 'cloud':
+            elif self.edition == "cloud":
                 self.client = SonarCloudClient(sonarcloud_url=self.url, username=self.username, password=self.password)
             else:
                 self.client = SonarQubeClient(sonarqube_url=self.url, username=self.username, password=self.password)
 
         if token:
-            if self.edition == 'community':
+            if self.edition == "community":
                 self.client = SonarQubeClient(sonarqube_url=self.url, token=self.token)
-            elif self.edition == 'enterprise':
+            elif self.edition == "enterprise":
                 self.client = SonarEnterpriseClient(sonarqube_url=self.url, token=self.token)
-            elif self.edition == 'cloud':
+            elif self.edition == "cloud":
                 self.client = SonarCloudClient(sonarcloud_url=self.url, token=self.token)
             else:
                 self.client = SonarQubeClient(sonarqube_url=self.url, token=self.token)
@@ -42,34 +43,36 @@ class SonarqubeBase(object):
             "MINOR": 3
         }
 
-        for issue in issues:
-            rule = self.client.rules.get_rule(key=issue["rule"])
+        with click.progressbar(issues) as _issues:
+            for issue in _issues:
 
-            if rule["rule"]["type"] == "VULNERABILITY":
+                rule = self.client.rules.get_rule(key=issue["rule"])
 
-                timestamp = issue["creationDate"]
-                desc = rule["rule"]["htmlDesc"]
-                severity =  rule["rule"]["severity"]
+                if rule["rule"]["type"] == "VULNERABILITY":
 
-                m_cwe = re.search("CWE-[0-9]+", desc)
-                m_cve = re.search("CVE-[0-9]+", desc)
-                m_capec = re.search("CAPEC-[0-9]+", desc)
-                readable_list = list(set([m_cwe.group(0) if m_cwe else None,
-                                                 m_cve.group(0) if m_cve else None,
-                                                 m_capec.group(0) if m_capec else None]))
+                    timestamp = issue["creationDate"]
+                    desc = rule["rule"]["htmlDesc"]
+                    severity =  rule["rule"]["severity"]
 
-                vulnerability_obj = {
-                    "title": issue["message"],
-                    "severity": severity_mapping[severity],
-                    "timestamp": timestamp,
-                    "vulnerabilities": [x for x in readable_list if x is not None],
-                    "vulnerability_types": {
-                        "cwe": m_cwe.group(0).split("-")[1] if m_cwe else "",
-                        "cve": m_cve.group(0).split("-")[1] if m_cve else "",
-                        "capec": m_capec.group(0).split("-")[1] if m_capec else ""
+                    m_cwe = re.search("CWE-[0-9]+", desc)
+                    m_cve = re.search("CVE-[0-9]+", desc)
+                    m_capec = re.search("CAPEC-[0-9]+", desc)
+                    readable_list = list(set([m_cwe.group(0) if m_cwe else None,
+                                                     m_cve.group(0) if m_cve else None,
+                                                     m_capec.group(0) if m_capec else None]))
+
+                    vulnerability_obj = {
+                        "title": issue["message"],
+                        "severity": severity_mapping[severity],
+                        "timestamp": timestamp,
+                        "vulnerabilities": [x for x in readable_list if x is not None],
+                        "vulnerability_types": {
+                            "cwe": m_cwe.group(0).split("-")[1] if m_cwe else "",
+                            "cve": m_cve.group(0).split("-")[1] if m_cve else "",
+                            "capec": m_capec.group(0).split("-")[1] if m_capec else ""
+                        }
                     }
-                }
 
-                vulnerabilities.append(vulnerability_obj)
+                    vulnerabilities.append(vulnerability_obj)
 
         return vulnerabilities
